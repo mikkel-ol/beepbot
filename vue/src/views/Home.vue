949b725157b81aa3f920b3c3710dea1f24f2e4da
Mikkel O. Laursen <m@ikkel.dk>
2019-07-30 11:12:02 +0200

    Add logout button

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -92,35 +92,41 @@
             </div>
           </div>
         </div>
-        <div v-if="connected" id="connection-status-container">
-          <div id="connection-status-bars">
-            <!-- Ping hover effect (Client.ping) -->
-          </div>
-          <div id="connection-status-description"></div>
-          <button id="disconnect-button" v-on:click="stop()">
-            <svg
-              class="buttonIcon-3yYVOH"
-              name="Nova_CallLeave"
-              aria-hidden="false"
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M21.1169 1.11603L22.8839 2.88403L19.7679 6.00003L22.8839 9.11603L21.1169 10.884L17.9999 7.76803L14.8839 10.884L13.1169 9.11603L16.2329 6.00003L13.1169 2.88403L14.8839 1.11603L17.9999 4.23203L21.1169 1.11603ZM18 22H13C6.925 22 2 17.075 2 11V6C2 5.447 2.448 5 3 5H7C7.553 5 8 5.447 8 6V10C8 10.553 7.553 11 7 11H6C6.063 14.938 9 18 13 18V17C13 16.447 13.447 16 14 16H18C18.553 16 19 16.447 19 17V21C19 21.553 18.553 22 18 22Z"
-              />
-            </svg>
-          </button>
+      </div>
+
+      <div v-if="connected" id="connection-status-container">
+        <div id="connection-status-bars">
+          <!-- Ping hover effect (Client.ping) -->
         </div>
+        <div id="connection-status-description">Bot connected</div>
+        <button id="disconnect-button" v-on:click="stop()">
+          <svg
+            class="buttonIcon-3yYVOH"
+            name="Nova_CallLeave"
+            aria-hidden="false"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M21.1169 1.11603L22.8839 2.88403L19.7679 6.00003L22.8839 9.11603L21.1169 10.884L17.9999 7.76803L14.8839 10.884L13.1169 9.11603L16.2329 6.00003L13.1169 2.88403L14.8839 1.11603L17.9999 4.23203L21.1169 1.11603ZM18 22H13C6.925 22 2 17.075 2 11V6C2 5.447 2.448 5 3 5H7C7.553 5 8 5.447 8 6V10C8 10.553 7.553 11 7 11H6C6.063 14.938 9 18 13 18V17C13 16.447 13.447 16 14 16H18C18.553 16 19 16.447 19 17V21C19 21.553 18.553 22 18 22Z"
+            />
+          </svg>
+        </button>
       </div>
 
       <div id="user-bar" v-if="user">
-        <img :src="`https://cdn.discordapp.com/avatars/${this.user.id}/a_a765d9f18091817029760eb739c1ec1d.webp?size=256`" alt=" ">
+        <img
+          :src="`https://cdn.discordapp.com/avatars/${this.user.id}/a_a765d9f18091817029760eb739c1ec1d.webp?size=256`"
+          alt=" "
+        />
         <div id="name-tag">
-          <div id="username"><span>{{this.user.username}}</span></div>
+          <div id="username">
+            <span>{{this.user.username}}</span>
+          </div>
           <div id="user-tag">#{{this.user.tag}}</div>
         </div>
         <button @click="logout()">Log out</button>
@@ -214,7 +220,7 @@ export default {
     },
 
     logout: function() {
-      ApiService.logout();
+      this.$router.push("/logout");
     }
   },
   async mounted() {
