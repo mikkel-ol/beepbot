fecd4699545cf495cb6fd8b64107104c5e569ead
Mikkel O. Laursen <m@ikkel.dk>
2019-08-05 16:27:53 +0200

    Disable HTTPS temporary while hit by certbot limits

--- express/index.js
+++ express/index.js
@@ -86,22 +86,25 @@ module.exports = {
 
 		switch (process.env.NODE_ENV) {
 			case 'production':
-				const credentials = {
-					key: fs.readFileSync(config.cert.privatekey),
-					cert: fs.readFileSync(config.cert.certificate),
-					ca: fs.readFileSync(config.cert.authority)
-				};
-				webServer = https.createServer(credentials, server);
+				// ! TEMP - DUE TO CERTBOT LIMITS
+				// const credentials = {
+				// 	key: fs.readFileSync(config.cert.privatekey),
+				// 	cert: fs.readFileSync(config.cert.certificate),
+				// 	ca: fs.readFileSync(config.cert.authority)
+				// };
+				// webServer = https.createServer(credentials, server);
 
-				// Redirect HTTP to HTTPS
-				http
-					.createServer(function(req, res) {
-						res.writeHead(301, {
-							Location: 'https://' + req.headers['host'].replace(8080, config.port) + req.url
-						});
-						res.end();
-					})
-					.listen(8080);
+				// // Redirect HTTP to HTTPS
+				// http
+				// 	.createServer(function(req, res) {
+				// 		res.writeHead(301, {
+				// 			Location: 'https://' + req.headers['host'].replace(8080, config.port) + req.url
+				// 		});
+				// 		res.end();
+				// 	})
+				// 	.listen(8080);
+
+				webServer = http.createServer(server);
 
 				break;
 
